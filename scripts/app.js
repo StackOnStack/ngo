bc95be90f75d0363f9012146b7760dc642228953
K I N G <nelsonmichael4real@gmail.com>
2021-04-01 21:10:19 +0800

    added favicon and fixed mobile navbar

--- scripts/app.js
+++ scripts/app.js
@@ -1,5 +1,3 @@
-const bodyTag = document.getElementsByTagName("body");
-
 const hamburgerMenu = document.getElementById("hamburger-menu");
 
 const cancelMenu = document.getElementById("cancel-menu");
@@ -9,9 +7,18 @@ const navBar = document.getElementById("mobile-navbar");
 hamburgerMenu.addEventListener("click", function(){
     hamburgerMenu.style.visibility="hidden";
     navBar.style.display="block";
-    bodyTag.style.overflow = "hidden";
 });
 
 cancelMenu.addEventListener("click", function(){
     navBar.style.display="none";
+   
 });
+
+let slow = setInterval(()=>{
+    if(window.screen.width >768){
+        hamburgerMenu.style.visibility="hidden";
+        console.log("hello");
+    }else{
+        hamburgerMenu.style.visibility="visible";
+    }
+}, 10);
