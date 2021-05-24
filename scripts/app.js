b78116982007e09d1e9921784c1e191cdc4017f3
K I N G <nelsonmichael4real@gmail.com>
2021-05-24 11:44:02 +0800

    fixed mobile navigation

--- scripts/app.js
+++ scripts/app.js
@@ -3,6 +3,16 @@ const hamburgerMenu = document.getElementById("hamburger-menu");
 const cancelMenu = document.getElementById("cancel-menu");
 
 const navBar = document.getElementById("mobile-navbar");
+/////////////////////////////////////////////////////////////////
+
+const aboutUs = document.getElementById('about-us-link');
+
+const stories = document.getElementById('stories-link');
+
+const blog = document.getElementById('blog-link');
+
+const contact = document.getElementById('contact-link');
+
 
 hamburgerMenu.addEventListener("click", function(){
     hamburgerMenu.style.visibility="hidden";
@@ -13,6 +23,26 @@ cancelMenu.addEventListener("click", function(){
     navBar.style.display="none";
    
 });
+
+aboutUs.addEventListener('click', ()=>{
+    navBar.style.display="none";
+    aboutUs.href="#about";
+})
+
+stories.addEventListener('click', ()=>{
+    navBar.style.display="none";
+    stories.href="#success-stories";
+})
+
+blog.addEventListener('click', ()=>{
+    navBar.style.display="none";
+    blog.href="#blog";
+})
+
+contact.addEventListener('click', ()=>{
+    navBar.style.display="none";
+    contact.href="#footer";
+})
 
 let slow = setInterval(()=>{
     if(window.screen.width >768){
